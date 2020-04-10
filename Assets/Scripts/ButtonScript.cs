a9d6a4b43e4b62f516bf1bfb689d5860fedeba2b
djb7559 <djb7559@rit.edu>
2020-04-10 19:00:31 -0400

    Working script for scene changing and basic proof of concept

--- Assets/Scripts/ButtonScript.cs
+++ Assets/Scripts/ButtonScript.cs
@@ -1,23 +1,10 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 using UnityEngine.SceneManagement;
-using UnityEngine.UI;
 
 public class ButtonScript : MonoBehaviour
 {
-    public void OnMouseUp()
+    public void ButtonMoveScene(string scene)
     {
-        SceneManager.LoadSceneAsync(1);
-    }
-
-    public void OnMouseEnter()
-    {
-        this.GetComponent<Text>().color = Color.black;
-    }
-
-    public void OnMouseExit()
-    {
-        this.GetComponent<Text>().color = Color.white;
+        SceneManager.LoadScene(scene);
     }
 }
