0370e59a32b7cb548fd7321f73e6f7fc04299159
djb7559 <djb7559@rit.edu>
2020-04-09 20:57:34 -0400

    Made a rough menu and working on button scripts

---
+++ Assets/Scripts/ButtonScript.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class ButtonScript : MonoBehaviour
+{
+    public void OnMouseUp()
+    {
+        SceneManager.LoadSceneAsync(1);
+    }
+
+    public void OnMouseEnter()
+    {
+        this.GetComponent<Text>().color = Color.black;
+    }
+
+    public void OnMouseExit()
+    {
+        this.GetComponent<Text>().color = Color.white;
+    }
+}
