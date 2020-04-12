4b8618afe89bf4ab4d447e397ad9cff8377d0ca4
Corey Rigney <coreyrigney@gmail.com>
2020-04-11 19:48:24 -0500

    Imported standard assets, updated terrain, added scripts for quiz generation and player core gameplay. Flight needs work.

---
+++ Assets/Scripts/QuestionGenerator.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class QuestionGenerator : MonoBehaviour
+{
+    public GameObject GoalRed;
+    public GameObject GoalBlue;
+    public GameObject GoalGreen;
+    public GameObject GoalYellow;
+    public GameObject Platform;
+    public GameObject Sign;
+    public GameObject player;
+    public int questionAmount;
+    // Start is called before the first frame update
+    void Start()
+    {
+        for(int i = 0; i < questionAmount; i++)
+        {
+            Vector3 position = player.transform.position + (i * new Vector3(0, -10, 40));
+            Instantiate(Platform, position, Quaternion.identity);
+            //Instantiate(Sign, new Vector3(position.x, position.y, position.z + 20), Quaternion.identity);
+            Instantiate(GoalBlue, new Vector3(position.x - 20, position.y + 5, position.z + 20), Quaternion.identity);
+            Instantiate(GoalRed, new Vector3(position.x - 5, position.y + 5, position.z + 20), Quaternion.identity);
+            Instantiate(GoalGreen, new Vector3(position.x + 5, position.y + 5, position.z + 20), Quaternion.identity);
+            Instantiate(GoalYellow, new Vector3(position.x + 20, position.y + 5, position.z + 20), Quaternion.identity);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
