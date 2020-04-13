14097252e7db2e79bc61a5ddfbda1780951c8d4d
Corey Rigney <coreyrigney@gmail.com>
2020-04-12 20:51:33 -0500

    Linked UI scenes to gameplay

---
+++ Assets/Scripts/QuitGame.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class QuitGame : MonoBehaviour
+{
+    public void quit()
+    {
+        Application.Quit();
+    }
+
+    public void ButtonMoveScene(string scene)
+    {
+        SceneManager.LoadScene(scene);
+    }
+}
