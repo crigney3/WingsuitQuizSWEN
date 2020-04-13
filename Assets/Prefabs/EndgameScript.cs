353055028874c736a267f0d5a90ab18e80ba5658
msc9169 <43228162+msc9169@users.noreply.github.com>
2020-04-12 22:07:31 -0400

    Endgame feedback popup added

---
+++ Assets/Prefabs/EndgameScript.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+
+public class EndgameScript : MonoBehaviour
+{
+    public GameObject EndgameFeedBack;
+    public bool[] questions;
+
+    public void OnCollisionEnter(Collision collision)
+    {
+        EndgameFeedBack.SetActive(true);
+    }
+}
