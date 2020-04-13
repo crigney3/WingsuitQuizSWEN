9aa89fd2ec02296b964451eddb77976ddf8de2b9
djb7559 <djb7559@rit.edu>
2020-04-12 21:19:40 -0400

    Feedback prefab

---
+++ Assets/Scripts/FeedbackSript.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class FeedbackSript : MonoBehaviour
+{
+    public GameObject feedbackWindow;
+    public Text Text;
+
+    public void OnCollisionEnter(Collision collision, string info)
+    {
+        feedbackWindow.SetActive(true);
+        Text.text = info;
+    }
+}
